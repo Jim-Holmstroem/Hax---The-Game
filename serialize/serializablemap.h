Jim-Holmstroem/Hax---The-Game

commit 89c3410bd1f3ecff851c86c9da81ff913e7f9a60

fixed error if T was a pure virtual class in partial specialized template for pointers

serialize/serializablemap.h
@@ -281,7 +281,7 @@ template < class Key, class T>
   public:
     virtual std::string getType() const
     {
-        return "SerializableMap<"+key_type().getType()+","+T().getType()+"*>";
+        return "SerializableMap<"+key_type().getType()+","+mapped_type()->getType()+"*>";
     };
 };
 

serialize/serializablevector.h
@@ -153,25 +153,25 @@ template < class T >
     class SerializableVector<T*> : public ISerializableContainer<T*>
 {
   protected:
-    std::vector<T*> content;
+    std::vector<T*,std::allocator<T*> > content;
 
   public:
-    typedef typename std::vector<T*>::value_type value_type;
-    typedef typename std::vector<T*>::allocator_type allocator_type;
+    typedef typename std::vector<T*,std::allocator<T*> >::value_type value_type;
+    typedef typename std::vector<T*,std::allocator<T*> >::allocator_type allocator_type;
 
-    typedef typename std::vector<T*>::size_type size_type;
-    typedef typename std::vector<T*>::difference_type difference_type;
+    typedef typename std::vector<T*,std::allocator<T*> >::size_type size_type;
+    typedef typename std::vector<T*,std::allocator<T*> >::difference_type difference_type;
 
-    typedef typename std::vector<T*>::reference reference;
-    typedef typename std::vector<T*>::const_reference const_reference;
+    typedef typename std::vector<T*,std::allocator<T*> >::reference reference;
+    typedef typename std::vector<T*,std::allocator<T*> >::const_reference const_reference;
 
-    typedef typename std::vector<T*>::pointer pointer;
-    typedef typename std::vector<T*>::const_pointer const_pointer;
+    typedef typename std::vector<T*,std::allocator<T*> >::pointer pointer;
+    typedef typename std::vector<T*,std::allocator<T*> >::const_pointer const_pointer;
 
-    typedef typename std::vector<T*>::iterator iterator;
-    typedef typename std::vector<T*>::const_iterator const_iterator;
-    typedef typename std::vector<T*>::reverse_iterator reverse_iterator;
-    typedef typename std::vector<T*>::const_reverse_iterator const_reverse_iterator;
+    typedef typename std::vector<T*,std::allocator<T*> >::iterator iterator;
+    typedef typename std::vector<T*,std::allocator<T*> >::const_iterator const_iterator;
+    typedef typename std::vector<T*,std::allocator<T*> >::reverse_iterator reverse_iterator;
+    typedef typename std::vector<T*,std::allocator<T*> >::const_reverse_iterator const_reverse_iterator;
 
     explicit SerializableVector(const allocator_type& alloc = allocator_type()):content(alloc){};
     explicit SerializableVector(size_type n,const_reference value = value_type(),const allocator_type& alloc = allocator_type()):content(n,value){};
@@ -247,7 +247,7 @@ template < class T >
     virtual void ToString(std::ostream& out) const
     {
         std::cout << "using partial specialized SerializableVector<T*>::ToString..." << std::endl;
-        SerializableVector<T*>::const_iterator it = content.begin();
+        SerializableVector<T*,std::allocator<T*> >::const_iterator it = content.begin();
         for(; it!=content.end(); ++it)
         {
             out << "# ";
@@ -268,7 +268,7 @@ template < class T >
   public:
     virtual std::string getType() const
     {
-        return "SerializableVector<"+T().getType()+"*>";
+        return "SerializableVector<"+value_type()->getType()+"*>";
     };
 
 };

commit 44ff59fe9ae597da19923e5661c21dfe5d997322

fixed segfault caused by https://github.com/Jim-Holmstroem/Hax---The-Game/commit/89c3410bd1f3ecff851c86c9da81ff913e7f9a60

serialize/serializablemap.h
@@ -281,7 +281,10 @@ template < class Key, class T>
   public:
     virtual std::string getType() const
     {
-        return "SerializableMap<"+key_type().getType()+","+mapped_type()->getType()+"*>";
+        T* mappedObj = new T();
+        std::string mappedObjType = mappedObj->getType();
+        delete mappedObj;
+        return "SerializableMap<"+key_type().getType()+","+mappedObjType+"*>";
     };
 };
 

serialize/serializablevector.h
@@ -268,7 +268,10 @@ template < class T >
   public:
     virtual std::string getType() const
     {
-        return "SerializableVector<"+value_type()->getType()+"*>";
+        T* obj = new T();
+        std::string type = obj->getType();
+        delete obj;
+        return "SerializableVector<"+type+"*>";
     };
 
 };
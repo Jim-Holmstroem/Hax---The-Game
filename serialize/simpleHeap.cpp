14f7da398698890622e410f41135adbb6f3107f8
Simon Ye <simon.m.ye@gmail.com>
2013-01-15 23:22:08 +0100

    data for class Coin missing from simpleHeap.cpp

--- serialize/simpleHeap.cpp
+++ serialize/simpleHeap.cpp
@@ -5,9 +5,10 @@
 #include "../ch/undead.h"
 #include "../area/indoor.h"
 #include "../area/outdoor.h"
+#include "../area/road.h"
 #include "../obj/container.h"
 #include "../obj/obstacle.h"
-#include "../area/road.h"
+#include "../obj/coin.h"
 
 hax::ISerializable* hax::allocateData(std::string type, std::string name)
 {
@@ -20,12 +21,13 @@ hax::ISerializable* hax::allocateData(std::string type, std::string name)
     else if(type == "castle"){return new Castle();}
     else if(type == "school"){return new School();}
     else if(type == "shop"){return new Shop();}
+    else if(type == "road"){return new Road(name);}
+    else if(type == "door"){return new Door(name);}
+    else if(type == "hatch"){return new Hatch(name);}
     else if(type == "pocket"){return new Pocket();}
     else if(type == "wallet"){return new Wallet();}
     else if(type == "backpack"){return new Backpack();}
     else if(type == "key"){return new Key();}
-    else if(type == "road"){return new Road(name);}
-    else if(type == "door"){return new Door(name);}
-    else if(type == "hatch"){return new Hatch(name);}
+    else if(type == "coin"){return new Coin();}
     else{return NULL;}
 }
